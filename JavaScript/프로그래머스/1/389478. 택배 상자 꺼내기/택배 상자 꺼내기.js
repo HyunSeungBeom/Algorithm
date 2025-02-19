8a113990151661c535694fac4b40158ab397dced
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-02-19 19:16:19 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -0,0 +1,26 @@
+function solution(n, w, num) {
+  let boxs = Array.from({ length: w }, () => []);
+  let boxNum = 0;
+  let direction = 1;
+
+  while (boxNum < n) {
+    let indices =
+      direction === 1 ? [...Array(w).keys()] : [...Array(w).keys()].reverse();
+
+    for (let idx of indices) {
+      boxNum++;
+      boxs[idx].push(boxNum);
+      if (boxNum === n) break;
+    }
+    direction *= -1; // 방향 전환
+  }
+
+  for (let values of boxs) {
+    let index = values.indexOf(num);
+    if (index !== -1) {
+      return values.length - index;
+    }
+  }
+
+  return 0;
+}
