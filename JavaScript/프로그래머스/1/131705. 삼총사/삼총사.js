bd8e5fe4cb2f1ca4603f94500399aaa258ddf0c2
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-03-13 17:37:01 +0900

    [level 1] Title: 삼총사, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,20 @@
+function solution(number) {
+  var answer = 0;
+
+  if (number.length < 3) {
+    return 0;
+  }
+
+  for (var i = 0; i < number.length - 2; i++) {
+    for (var j = i + 1; j < number.length - 1; j++) {
+      for (var k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
