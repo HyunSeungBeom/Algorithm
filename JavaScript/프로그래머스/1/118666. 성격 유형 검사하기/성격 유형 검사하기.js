8037c04f11180a983e053503be248e7e947e90fd
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-03-13 16:42:07 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.86 ms, Memory: 33.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ JavaScript/프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,22 +1,4 @@
 function solution(survey, choices) {
-  let answer = "";
-  let scores = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
-
-  for (let i = 0; i < survey.length; i++) {
-    let [first, second] = survey[i].split("");
-    let choice = choices[i];
-
-    if (choice < 4) {
-      scores[first] += 4 - choice;
-    } else if (choice > 4) {
-      scores[second] += choice - 4;
-    }
-  }
-
-  answer += scores["R"] >= scores["T"] ? "R" : "T";
-  answer += scores["C"] >= scores["F"] ? "C" : "F";
-  answer += scores["J"] >= scores["M"] ? "J" : "M";
-  answer += scores["A"] >= scores["N"] ? "A" : "N";
-
-  return answer;
+    var answer = '';
+    return answer;
 }
