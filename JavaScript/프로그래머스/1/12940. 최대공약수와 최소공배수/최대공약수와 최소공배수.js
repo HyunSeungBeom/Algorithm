8828192b88592250105cf18272d46c431f8f841f
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-03-15 00:58:39 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,25 @@
+function solution(n, m) {
+  var answer = [];
+  var nMeasure = [];
+  var mMeasure = [];
+
+  for (var i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      nMeasure.push(i);
+    }
+  }
+
+  for (var j = 1; j <= m; j++) {
+    if (m % j === 0) {
+      mMeasure.push(j);
+    }
+  }
+
+  var commonMeasure = nMeasure.filter((value) => mMeasure.includes(value));
+  var gcd = Math.max(...commonMeasure);
+  var lcm = (n * m) / gcd;
+
+  answer.push(gcd, lcm);
+
+  return answer;
+}
