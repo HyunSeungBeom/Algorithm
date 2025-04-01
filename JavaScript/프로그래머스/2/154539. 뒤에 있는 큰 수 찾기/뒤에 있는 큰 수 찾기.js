638eeb4f51e5aa23cecac8e3e6e55192e19fea33
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-04-01 18:27:50 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 28.28 ms, Memory: 126 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+  const result = new Array(arr.length).fill(-1);
+  const stack = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    while (stack.length > 0 && stack[stack.length - 1] <= arr[i]) {
+      stack.pop();
+    }
+
+    if (stack.length > 0) {
+      result[i] = stack[stack.length - 1];
+    }
+
+    stack.push(arr[i]);
+  }
+
+  return result;
+}
