7afee4b6219fc65c5a5874195facb80a7c849f19
현승범 <62886997+HyunSeungBeom@users.noreply.github.com>
2025-04-08 16:30:31 +0900

    [level 2] Title: 귤 고르기, Time: 100.22 ms, Memory: 64.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ JavaScript/프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,12 +1,8 @@
 function solution(k, tangerine) {
-  const countMap = new Map();
+  let countMap = {};
 
-  for (let t of tangerine) {
-    countMap.set(t, (countMap.get(t) || 0) + 1);
-  }
-
-  const sortedCounts = [...countMap.values()].sort((a, b) => b - a);
-
+  tangerine.forEach((item) => (countMap[item] = (countMap[item] || 0) + 1));
+  const sortedCounts = Object.values(countMap).sort((a, b) => b - a);
   let total = 0;
   let kind = 0;
 
