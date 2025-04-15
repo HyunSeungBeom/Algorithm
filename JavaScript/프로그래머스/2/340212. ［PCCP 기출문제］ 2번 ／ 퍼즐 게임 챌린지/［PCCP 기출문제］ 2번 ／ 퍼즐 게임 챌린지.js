06f11ea204d110353837b4274b295479597bbb11
SeungBeom Hyun <62886997+HyunSeungBeom@users.noreply.github.com>
2025-04-15 19:41:50 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 33.47 ms, Memory: 55.8 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -0,0 +1,36 @@
+function solution(diffs, times, limit) {
+  let left = 1;
+  let right = 100000;
+  let answer = right;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (canSolve(diffs, times, limit, mid)) {
+      answer = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
+function canSolve(diffs, times, limit, level) {
+  let total = 0;
+
+  for (let i = 0; i < diffs.length; i++) {
+    if (diffs[i] <= level) {
+      total += times[i];
+    } else {
+      const retry = diffs[i] - level;
+      total += retry * (times[i] + times[i - 1]) + times[i];
+    }
+
+    if (total > limit) {
+      return false;
+    }
+  }
+
+  return true;
+}
