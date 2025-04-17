1623058851a1fbfacf232be619e64d5f81e7566a
SeungBeom Hyun <62886997+HyunSeungBeom@users.noreply.github.com>
2025-04-17 18:59:15 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.23 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  let count = 0;
+  let removedZero = 0;
+
+  while (s !== "1") {
+    let beforeLength = s.length;
+    s = s.replace(/0/g, "");
+    removedZero += beforeLength - s.length;
+
+    s = s.length.toString(2);
+    count++;
+  }
+
+  return [count, removedZero];
+}
