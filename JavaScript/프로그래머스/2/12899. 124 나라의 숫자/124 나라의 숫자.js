4a37cfd83e176a7db6e0346341ee4b076f3d2d17
SeungBeom Hyun <62886997+HyunSeungBeom@users.noreply.github.com>
2025-04-19 19:39:44 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  let answer = "";
+
+  while (n > 0) {
+    let remainder = n % 3;
+
+    if (remainder === 0) {
+      answer = "4" + answer;
+      n = Math.floor(n / 3) - 1;
+    } else {
+      answer = remainder + answer;
+      n = Math.floor(n / 3);
+    }
+  }
+
+  return answer;
+}
